3e69d1000a2f172b395d61da905ffc6e456a712c
fionnmcconville555@gmail.com <fionnmcconville555@gmail.com>
2024-12-11 16:16:35 +0000

    challenege 3

---
+++ models/mart/finance/finance_campaigns_day.sql
@@ -0,0 +1,18 @@
+select 
+f.date_date,
+round(f.operational_margin - c.ads_costs) as ads_margin,
+f.average_basket,
+f.operational_margin,
+c.ads_costs,
+c.ads_impression,
+c.ads_clicks,
+f.quantity,
+f.revenue,
+f.purchase_cost,
+f.margin,
+f.shipping_fee,
+f.logcost,
+f.ship_cost
+from {{ref("finance_days")}} f
+left join {{ref("int_campaigns_day")}} c
+on c.date_date = f.date_date
